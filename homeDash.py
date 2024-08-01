84a00fc19764fc456f1a9ec80037bfa81f0e96d4
JimmyKnox2058 <114237293+JimmyKnox2058@users.noreply.github.com>
2024-08-01 16:23:43 +0200

    homeDash improvements

--- homeDash.py
+++ homeDash.py
@@ -15,7 +15,7 @@ os.chdir(BASE_DIR)
 app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
+    html.H1('Wortanalyse der gemeinsamen Ministerialblätter'),
     html.Div([
         html.Div(
             dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
--- pages/home.py
+++ pages/home.py
@@ -11,6 +11,11 @@ from dash import html
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
+    html.H2('Das Projekt ist ein Lernprojekt, dessen Inhalt die Wortanalyse von ca. 2700 Ausgaben des gemeinsamen Ministerialblattes ist. Die gemeinsamen Ministerialblätter sind die seit 1950 von der Bundesregierung Deutschlands herausgegebenen Verwaltungsanweisungen - diese wurden auf der Website fragdenstaat.de als .pdf veröffentlicht.'),
+    html.Div(['Teilaspekte, mit denen wir uns auseinandergesetzt haben, sind:', html.Br(), 
+            html.Li("Beschaffung, Bereinigung und Verarbeitung der Textdaten"),  
+            html.Li("Natural Language Processing"), 
+            html.Li("statistische Methoden und Topic Modelling"), 
+            html.Li("Darstellung der Ergebnisse mit Word Clouds, Graphen und Tabellen in einem Dashboard")
+             ]),
 ])
